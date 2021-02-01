b18041e5cbfebaf120c1be24a3b199359529f61e
XXXNELL <xxxxxnell@gmail.com>
2021-02-01 09:41:54 +0900

    Update styles

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -7,13 +7,13 @@ $(document).ready(function() {
   }
 
   // Zooming
-  new Zooming(
-    {customSize: '100%', scaleBase: 0.9, scaleExtra: 0}
-  ).listen('.zooming');
+  // new Zooming(
+  //   {customSize: '100%', scaleBase: 0.9, scaleExtra: 0}
+  // ).listen('.zooming');
 
   // Share buttons
-  $('.article-share a').on('click', function() {
-    window.open($(this).attr('href'), 'Share', 'width=200,height=200,noopener');
-    return false;
-  });
+  // $('.article-share a').on('click', function() {
+  //   window.open($(this).attr('href'), 'Share', 'width=200,height=200,noopener');
+  //   return false;
+  // });
 });
